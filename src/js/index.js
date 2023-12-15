e2f10987e86ef85544c2d98bcfdf0ad1590b0c73
MaisonBertrand <93873899+MaisonBertrand@users.noreply.github.com>
2023-12-14 19:30:24 -0600

    Init Commit

---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+//let imageContainer = document.getElementById(imageContainer);
+// var leftButton = document.getElementById(leftButton)
+
+document.body.style.backgroundImage = "url('./DSC03222.jpg')";
+
+
+// window.addEventListener("DOMContentLoaded", (event) => {
+//     const rightButton = document.getElementById('rightButton');
+//     if (rightButton) {
+//         rightButton.addEventListener('click', (
+//             imageContainer.style.backgroundImage= "url('../images/DSC03224')"
+//             ), false);
+//     }
+// });
+
+
+
+// var images = ['../images/DSC03224','../images/DSC03581','../images/DSC03228']
+// var slideTime = 2000;
+
+// rightButton.cycleUp = false;
+
+// rightButton.addEventListener('click',function(event){
+//     if(this.cycleUp) return;
+//     this.cycleUp = true;
+
+//     var imageNumber = 0;
+//     var newInterval = setInterval(function(){
+
+//         imageContainer.setAttribute('src',images[imageNumber]);
+
+//         if(imageNumber==images.length-1){
+//             imageNumber = 0;
+//         } else {
+//             imageNumber++;
+//         }
+//     },slideTime);
+
+// }); 
+
+// rightButton.addEventListener('click', changePictureRight);
+// leftButton.addEventListener('click', changePictureRight);
+
+
